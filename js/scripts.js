47128f96e454c7f14fdb2ed0a0650953e687a8f0
Joshua Merrell <84419054+joshmerrell@users.noreply.github.com>
2025-04-28 07:29:26 +0000

    Update style for a photo frame on colored wall look

--- js/scripts.js
+++ js/scripts.js
@@ -17,15 +17,19 @@ fetch(url)
     const roster = document.querySelector(".roster");
 
     // loop through data
-    data.forEach( student => {
+    data.forEach(student => {
       
       // template
-      const template = `
-          <figure>
-            <figcaption> ${student.name} </figcaption>
-            <img src=" ${student.imageUrl} " alt=" ${student.name} ">
-          </figure>
-       `;
+      const template = `<figure class="card" style="background-color: ${student.favoriteColor};">
+            <div class="card-inner">
+              <img src=" ${student.imageUrl} " alt=" ${student.name} ">
+              <figcaption>
+                <h2>${student.name} - ${student.status}</h2>
+                <p class="cursive">Motto: ${student.motto}</p>
+                <p class="cursive">Favorite Character: ${student.favoriteSimpsonsCharacter}</p>
+              </figcaption>
+            </div>
+          </figure>`;
 
       // insert EACH `student` record into container
       roster.insertAdjacentHTML("afterbegin", template);
